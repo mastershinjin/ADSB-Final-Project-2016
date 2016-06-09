c36fb9e1c711a164db46ba009728387eff7b50b5
morchellaesculenta <wendyshi01@gmail.com>
2016-06-09 14:27:22 -0400

    Add files via upload

--- View.java
+++ View.java
@@ -1,36 +1,83 @@
-
-
 import java.io.File;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.FontPosture;
+import javafx.scene.image.Image;
 
-public class View extends Application{
-	public static void main(String[] args){
-		launch(args);
-	}
-	public void start(Stage primaryStage){
-		primaryStage.setTitle("Uso");
+public class View extends Application 
+{
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage theStage) {
+    	
+    	//The music playing part of the code (it works)
+    	theStage.setTitle("Hello World!");
+
+    	String musicFile = "sample2.mp3";     // For example
+
+    	Media sound = new Media(new File(musicFile).toURI().toString());
+    	MediaPlayer mediaPlayer = new MediaPlayer(sound);
+    	mediaPlayer.play();
+    
+    	//The animation part of the code (it works)
+        Group root = new Group();
+        Scene theScene = new Scene(root);
+        theStage.setScene( theScene );
+        
+
+        Canvas canvas = new Canvas( 512, 512 );
+        root.getChildren().add( canvas );
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Image earth = new Image( "earth.png" );
+        Image sun   = new Image( "sun.png" );
+        Image space = new Image( "space.png" );
+
+        final long startNanoTime = System.nanoTime();
+
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0; 
+
+                double x = 232 + 128 * Math.cos(t);
+                double y = 232 + 128 * Math.sin(t);
+
+                // Clear the canvas
+                gc.clearRect(0, 0, 512,512);
+
+                // background image clears canvas
+                gc.drawImage( space, 0, 0 );
+                gc.drawImage( earth, x, y );
+                gc.drawImage( sun, 196, 196 );
+            }
+        }.start();
 		
-		Group root = new Group();
-		Scene theScene = new Scene(root);
-		primaryStage.setScene(theScene);
 		
-		Canvas canvas = new Canvas(400, 200);
 		
-		String path = "USB20FD/CS/musicFiles/sample1";
-		Media blah = new Media(new File(path).toURI().toString());
-		MediaPlayer player = new MediaPlayer(blah);
-		MediaView view = new MediaView(player);
-		view.getMediaPlayer().play();
-		
-;		primaryStage.show();
-		
-	}
+        theStage.show();
+        
+        
+    }
 }
+
