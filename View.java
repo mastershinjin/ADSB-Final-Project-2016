9b6daa32c4b22b83527f844aa3f1a36bee9506e4
morchellaesculenta <wendyshi01@gmail.com>
2016-06-17 21:52:11 -0400

    Update View.java

--- View.java
+++ View.java
@@ -1,7 +1,10 @@
+import java.awt.Graphics;
 import java.io.File;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
@@ -16,10 +19,14 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.FontPosture;
+import javafx.scene.text.Text;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 
 public class View extends Application 
 {
+	int points=0;
+	
     public static void main(String[] args) 
     {
         launch(args);
@@ -31,8 +38,7 @@ public class View extends Application
     	//The music playing part of the code (it works)
     	theStage.setTitle("Uso!");
 
-    	String musicFile = "Nightcore - Infinity.mp3";
-    	//http://mp3songx.com/4rdce/download-Nightcore---Infinity-mp3.html
+    	String musicFile = "Nightcore - Infinity.mp3";     // For example
 
     	Media sound = new Media(new File(musicFile).toURI().toString());
     	MediaPlayer mediaPlayer = new MediaPlayer(sound);
@@ -43,17 +49,30 @@ public class View extends Application
         Scene theScene = new Scene(root);
         theStage.setScene( theScene );
         
-
+        
+		
         Canvas canvas = new Canvas( 1272, 817 );
         root.getChildren().add( canvas );
+        
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        Image earth = new Image( "earth.png" );
+        //Adds background images and buttons to the scene
         Image space = new Image( "usobackground1.jpg" );
         MusicButton button = new MusicButton(196,196);
+        MusicButton button1 = new MusicButton(196,196);
+        MusicButton button2 = new MusicButton(196,196);
+        MusicButton button3 = new MusicButton(196,196);
+        MusicButton button4 = new MusicButton(196,196);
+        MusicButton button5 = new MusicButton(196,196);
+        MusicButton button6 = new MusicButton(196,196);
+        MusicButton button7 = new MusicButton(196,196);
+        MusicButton button8 = new MusicButton(196,196);
+        MusicButton button9 = new MusicButton(196,196);
+        MusicButton button10 = new MusicButton(196,196);
  
-
+        
+		int var1 = points;
         final long startNanoTime = System.nanoTime();
 
         new AnimationTimer()
@@ -70,8 +89,87 @@ public class View extends Application
 
                 // background image clears canvas
                 gc.drawImage( space, 0, 0 );
-                gc.drawImage( earth, x, y );
-                gc.drawImage( button.getImage(), 196, 196);
+                
+                gc.drawImage( button.getImage(), x, y);
+                
+                
+                gc.drawImage( button1.getImage(), x+100, y+500);
+                
+                /*
+                gc.drawImage( button2.getImage(), x+525, y+605);
+                gc.drawImage( button3.getImage(), x+700, y+1000);
+                gc.drawImage( button4.getImage(), x+500, y+90);
+                gc.drawImage( button5.getImage(), x-100, y-90);
+                gc.drawImage( button6.getImage(), x+700, y+60);
+                gc.drawImage( button7.getImage(), x-70, y-100);
+                gc.drawImage( button8.getImage(), x+10, y+15);
+                gc.drawImage( button9.getImage(), x+3, y+4);
+                gc.drawImage( button10.getImage(), x+5, y+11);
+                */
+                
+                
+                /*
+                for (int i=0; i<100; i++){
+                	MusicButton button1 = new MusicButton(Math.random()*1272, Math.random()*817);
+                	gc.drawImage(button1.getImage(),  x,  y);
+                }
+                */
+                
+                ScoreGauge sc = new ScoreGauge();
+        		
+                
+        		theScene.setOnMouseClicked(
+        				new EventHandler<MouseEvent>(){
+        					public void handle(MouseEvent e){
+        						if(button.hitCircle.contains(e.getX(), e.getY())){
+        							if(button.isAlive(sc)){
+        								button.disappear();
+        								sc.updateScore(10, gc);
+        								points+=20;
+        							}else{
+        								button.disappear();
+        								sc.updateScore(-15, gc);
+        								points+=20;
+        							}
+        						}
+        						MusicButton next = new MusicButton(100,100);
+        						
+        					}
+        				}
+        			);
+        		
+        		theScene.setOnMouseClicked(
+        				new EventHandler<MouseEvent>(){
+        					public void handle(MouseEvent e){
+        						if(button1.hitCircle.contains(e.getX(), e.getY())){
+        							if(button1.isAlive(sc)){
+        								button1.disappear();
+        								sc.updateScore(10, gc);
+        								points+=20;
+        							}else{
+        								button1.disappear();
+        								sc.updateScore(-15, gc);
+        								points+=20;
+        							}
+        						}
+        						MusicButton next = new MusicButton(100,100);
+        						
+        					}
+        				}
+        			);
+        		
+        		
+               
+        			/*
+        			Text pointsText = new Text (40, 40, "Number of points: " + points);
+            		pointsText.setFont(Font.font ("Verdana", 20));
+            		pointsText.setFill(Color.RED);
+            		root.getChildren().add(pointsText);
+        			root.getChildren().remove(pointsText);
+        			*/
+        		
+                gc.strokeText("Number of points: " + points, 50, 50);
+                
             }
         }.start();
 		
